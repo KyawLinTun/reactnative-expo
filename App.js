d018ae49d0338978daaaa0def18251a0538d8b27
KyawLinTun <kyawlintun.kk62@gmail.com>
2022-06-07 15:32:33 +0630

    Test react native using expo

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => (
+  // Replace `null` with the `View` component
+  <View style={{ flex:1,justifyContent:'center',alignItems:'center', font}}>
+    <View style={{width: 100,height:100, backgroundColor:'yellow',}}></View>
+    <View style={{width: 100,height:100, backgroundColor:'Green',}}></View>
+    <View style={{width: 100,height:100, backgroundColor:'red',}}></View>
+  </View>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
